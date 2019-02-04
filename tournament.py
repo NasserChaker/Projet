c581222ff4b0bbdcd4f19f16a80e1b33957b557e
NasserChaker <46898104+NasserChaker@users.noreply.github.com>
2019-02-04 19:05:10 +0100

    Create tournament.py

---
+++ tournament.py
@@ -0,0 +1,24 @@
+from socceria import StrikeStrategy , RandomStrategy
+from soccersimulator import SoccerTeam
+
+def get_team ( nb_players ):
+    team = SoccerTeam ( name = " Nasser’s ␣ Team " )
+    if nb_players == 1:
+        team.add("Attaquant",SimpleStrategy(gobetter,'Go - better'))
+    if nb_players == 2:
+        team.add("Attaquant",SimpleStrategy(gobetter,'Go'))
+        team.add("Défenseur",SimpleStrategy(defenseur2,'Def'))
+    return team
+
+if __name__ == ’ __main__ ’:
+    from soccersimulator import Simulation , show_simu
+    
+    # Check teams with 1 player and 2 players
+    team1 = get_team (1)
+    team2 = get_team (2)
+    
+    # Create a match
+    simu = Simulation ( team1 , team2 )
+    
+    # Simulate and display the match
+    show_simu ( simu )
