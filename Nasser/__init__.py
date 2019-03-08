a34a65d7cc7f969f6daf49aa26d987b7e56259a7
Alex Trouve <trouve.alex@free.fr>
2019-03-08 14:52:42 +0100

    tme7

--- Nasser/__init__.py
+++ Nasser/__init__.py
@@ -1,12 +1,10 @@
 from Nasser.strategies import defenseur2 , RandomStrategy , gobetter,attaquant2,one, gardien
 from soccersimulator import SoccerTeam
 from Nasser.tools import SimpleStrategy, SuperState, GoTestStrategy
-def get_team ( nb_players, nb ):
+def get_team (nb_players):
     team = SoccerTeam ( name = " Nasser’s ␣ Team " )
-    if (nb_players == 1) and (nb==1) :
-        team.add("go",SimpleStrategy(gardien,'Go'))
-    if (nb_players == 1) and (nb==2) :
-        team.add("one",SimpleStrategy(one,'One'))
+    if (nb_players == 1):
+        team.add("one",SimpleStrategy(one,'Nasser'))
     if nb_players == 2:
         team.add("Attaquant",SimpleStrategy(attaquant2,'att'))
         team.add("Défenseur",SimpleStrategy(defenseur2,'Def'))
--- tournament.py
+++ tournament.py
@@ -3,8 +3,8 @@ from Nasser import get_team
 from soccersimulator import Simulation , show_simu
     
 # Check teams with 1 player and 2 players
-team1 = get_team (1,2)
-team2 = get_team (1,1)
+team1 = get_team (1)
+team2 = get_team (1)
     
 # Create a match
 simu = Simulation ( team1 , team2 )
