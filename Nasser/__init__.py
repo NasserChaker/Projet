588283b8a509f07eff8566455ec2412b5d557d52
Alex Trouve <trouve.alex@free.fr>
2019-03-23 10:45:23 +0100

    tme7

--- Nasser/__init__.py
+++ Nasser/__init__.py
@@ -3,7 +3,7 @@ from soccersimulator import SoccerTeam
 from Nasser.tools import SimpleStrategy, SuperState, GoTestStrategy
 
 def get_team (nb_players):
-    team = SoccerTeam (name = " Nasser’s Team " )
+    team = SoccerTeam(name = "Nasser’s Team")
     if (nb_players == 1):
         team.add("One",SimpleStrategy(one,'One'))
     if nb_players == 2:
